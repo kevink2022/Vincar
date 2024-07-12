1673d688da17fe518b5ee6cd5470b61c81df29eb
kevink2022 <kevink2019@gmail.com>
2024-07-12 12:49:17 -0700

    Initial Commit

---
+++ Vincar/VincarApp.swift
@@ -0,0 +1,17 @@
+//
+//  VincarApp.swift
+//  Vincar
+//
+//  Created by Kevin Kelly on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VincarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
